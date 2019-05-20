69db57cff818cc4a5dd911eb97134729b6588447
HaiyanYao <haiyanyao1988@gmail.com>
2019-05-20 10:34:56 -0400

    assessment

--- src/store/index.js
+++ src/store/index.js
@@ -3,18 +3,18 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
 import sagas from "./sagas";
 import weatherReducer from "./reducers/Weather";
+import droneReducer from "./reducers/droneR";
 
 export default () => {
   const rootReducer = combineReducers({
-    weather: weatherReducer
+    weather: weatherReducer,
+    drone: droneReducer
   });
 
   const composeEnhancers = composeWithDevTools({});
   const sagaMiddleware = createSagaMiddleware();
   const middlewares = applyMiddleware(sagaMiddleware);
   const store = createStore(rootReducer, composeEnhancers(middlewares));
-
   sagas.forEach(sagaMiddleware.run);
-
   return store;
 };
